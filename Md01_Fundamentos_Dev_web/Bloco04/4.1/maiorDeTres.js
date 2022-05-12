82bbf63712c79220e1462b682e5dc8d12e05629b
Conan Goodwin <conan_goodwin@hotmail.com>
2022-05-12 18:49:47 -0300

    Criado maiorDeTres.js

---
+++ Md01_Fundamentos_Dev_web/Bloco04/4.1/maiorDeTres.js
@@ -0,0 +1,33 @@
+const num1 = 5, num2 =10, num3 = 2;
+
+if (num1 === num2) {
+    if (num1 === num3) {
+        console.log("Todos os numeros são iguais.");
+    }else if (num1 > num3) {
+        console.log("Numero maior: ",num1);
+    }else {
+        console.log("Numero maior: ",num3);
+    }
+}else if (num1 === num3) {
+    if (num1 > num2) {
+        console.log("Numero maior: ",num1);
+    } else {
+        console.log("Numero maior: ",num2);
+    }
+}else if (num2 === num3) {
+    if (num2 > num1) {
+        console.log("Numero maior: ",num2);
+    }else {
+        console.log("Numero maior: ",num1);
+    }
+}else if (num1 > num2) {
+    if (num1 > num3) {
+        console.log("Numero maior: ",num1);
+    }else {
+        console.log("Numero maior: ",num3);
+    }
+}else if (num2 > num3) {
+    console.log("Numero maior: ",num2);
+}else {
+    console.log("Numero maior: ",num3);
+}
