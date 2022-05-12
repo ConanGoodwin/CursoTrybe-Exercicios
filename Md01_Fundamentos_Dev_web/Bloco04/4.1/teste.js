6695c91398ca65daeeb94f8a0d297e1ba8cfdc68
Conan Goodwin <conan_goodwin@hotmail.com>
2022-05-12 15:31:04 -0300

    Testando variaveis no teste.js

---
+++ Md01_Fundamentos_Dev_web/Bloco04/4.1/teste.js
@@ -0,0 +1,12 @@
+let myName = "Conan";
+const birthCity = "Belo Horizonte";
+let birthYear = 1981;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+birthYear = 2030
+
+console.log(birthYear);
+// birthCity = "teste";
