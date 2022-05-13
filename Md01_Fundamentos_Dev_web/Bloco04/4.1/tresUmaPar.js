4756d292bfa93e7a5f003a8552c93c57e521f5f2
Conan Goodwin <conan_goodwin@hotmail.com>
2022-05-12 23:54:31 -0300

    Criado tresUmaPar.js

---
+++ Md01_Fundamentos_Dev_web/Bloco04/4.1/tresUmaPar.js
@@ -0,0 +1,8 @@
+const num1 = 7, num2 = 2, num3 = 5;
+
+if (( num1 % 2) * (num2 % 2) * (num3 % 2) === 0)
+{
+    console.log("Pelo menos um dos numeros é par.");
+}else {
+    console.log("Nenhum dos numeros é par.");
+}
