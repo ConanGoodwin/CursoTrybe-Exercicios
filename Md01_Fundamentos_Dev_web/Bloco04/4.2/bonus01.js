6a2601170b57c02bbb6adcdec3773cba47e2d0c8
Conan Goodwin <conan_goodwin@hotmail.com>
2022-05-13 19:16:29 -0300

    exercicios.js de acordo com o exercicio bonus

--- Md01_Fundamentos_Dev_web/Bloco04/4.2/bonus01.js
+++ Md01_Fundamentos_Dev_web/Bloco04/4.2/bonus01.js
@@ -1,25 +1,53 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let numbersPeloSeguinte = [];
+
+//parte 03
 
 for (let index = 0; index < numbers.length; index += 1) {
-  for (let index2 = 0; index2 < numbers.length; index2 += 1) {
-    if (numbers[index] < numbers[index2]) {
-      let extra = numbers[index];
-      numbers[index] = numbers[index2];
-      numbers[index2] = extra;
-    }
+  if (numbers[index+1]  === undefined) {
+    numbersPeloSeguinte.push(numbers[index] * 2);
+  }
+  else {
+    numbersPeloSeguinte.push(numbers[index] * numbers[index + 1]);
   }
 }
 
-console.log(numbers);
+console.log(numbersPeloSeguinte);
 
-for (let index = 0; index < numbers.length; index += 1) {
-  for (let index2 = 0; index2 < numbers.length; index2 += 1) {
-    if (numbers[index] > numbers[index2]) {
-      let extra = numbers[index];
-      numbers[index] = numbers[index2];
-      numbers[index2] = extra;
-    }
-  }
-}
+// for (numero of numbers) {
+//   numbersPeloSeguinte.push(numero);
+// }
 
-console.log(numbers);
+// for (let index = 0; index < numbers.length; index += 1) {
+//   for (let index2 = 0; index2 < numbers.length; index2 += 1) {
+//     if (numbers[index] < numbers[index2]) {
+//       let extra = numbers[index];
+//       numbers[index] = numbers[index2];
+//       numbers[index2] = extra;
+//     }
+//   }
+// }
+
+// console.log(numbers);
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//   for (let index2 = 0; index2 < numbers.length; index2 += 1) {
+//     if (numbers[index] > numbers[index2]) {
+//       let extra = numbers[index];
+//       numbers[index] = numbers[index2];
+//       numbers[index2] = extra;
+//     }
+//   }
+// }
+
+// console.log(numbers);
+
+// console.log(numbersPeloSeguinte);
+
+// for (let index = 0; index < numbersPeloSeguinte.length; index += 1) {
+//   if (numbersPeloSeguinte[index + 1] === undefined) {
+//     numbersPeloSeguinte[index] *= 2;
+//   } else {
+//     numbersPeloSeguinte[index] *= numbersPeloSeguinte[index + 1];
+//   }
+// }
