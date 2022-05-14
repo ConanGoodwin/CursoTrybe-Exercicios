c1caa67811e682a6daf1528a52641eaf5a0a000c
Conan Goodwin <conan_goodwin@hotmail.com>
2022-05-14 17:28:35 -0300

    Exercicio bonus 4

--- Md01_Fundamentos_Dev_web/Bloco04/4.3/formasGeometricas.js
+++ Md01_Fundamentos_Dev_web/Bloco04/4.3/formasGeometricas.js
@@ -1,6 +1,7 @@
-let qt = 5;
+let qt = 7;
 let saida = "";
-let saidaEspaco = "";
+let espaco = "";
+let asterisco = "";
 
 if (qt > 1) {
   // for (let coluna = 0; coluna < qt; coluna += 1) {
@@ -16,12 +17,24 @@ if (qt > 1) {
   //   console.log(saida);
   // }
 
-  for (let coluna = 0; coluna < qt; coluna += 1) {
-    saidaEspaco = "";
-    for (let linha = 0; linha < qt - coluna; linha += 1) {
-      saidaEspaco += " ";
+  // for (let coluna = 0; coluna < qt; coluna += 1) {
+  //   espaco = "";
+  //   for (let linha = 0; linha < qt - coluna; linha += 1) {
+  //     espaco += " ";
+  //   }
+  //   saida += "*";
+  //   console.log(espaco + saida);
+  // }
+
+  for (let coluna = 1; saida.length < qt; coluna += 1) {
+    espaco = "";
+    saida = "";
+    for (let linha = 0; linha < (qt/2) - coluna; linha += 1) {
+      espaco += " ";
     }
-    saida += "*";
-    console.log(saidaEspaco + saida);
+    for (let restLinha = 0; restLinha < qt - (espaco.length * 2); restLinha += 1) {
+      saida += "*";
+    }
+    console.log(espaco + saida + espaco);
   }
 }
