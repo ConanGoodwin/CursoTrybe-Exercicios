00e4a93072380b21046624f249befb7e18257bcd
Conan Goodwin <conan_goodwin@hotmail.com>
2022-05-14 19:17:21 -0300

    Exercicio bonus 5

--- Md01_Fundamentos_Dev_web/Bloco04/4.3/formasGeometricas.js
+++ Md01_Fundamentos_Dev_web/Bloco04/4.3/formasGeometricas.js
@@ -1,20 +1,19 @@
-let qt = 7;
-let saida = "";
+let qt = 9;
 let espaco = "";
 let asterisco = "";
 
 if (qt > 1) {
   // for (let coluna = 0; coluna < qt; coluna += 1) {
-  //   saida = "";
+  //   asterisco = "";
   //   for (let linha = 0; linha < qt; linha += 1) {
-  //     saida += "*";
+  //     asterisco += "*";
   //   }
-  //   console.log(saida);
+  //   console.log(asterisco);
   // }
 
   // for (let coluna = 0; coluna < qt; coluna += 1) {
-  //   saida += "*";
-  //   console.log(saida);
+  //   asterisco += "*";
+  //   console.log(asterisco);
   // }
 
   // for (let coluna = 0; coluna < qt; coluna += 1) {
@@ -22,19 +21,37 @@ if (qt > 1) {
   //   for (let linha = 0; linha < qt - coluna; linha += 1) {
   //     espaco += " ";
   //   }
-  //   saida += "*";
-  //   console.log(espaco + saida);
+  //   asterisco += "*";
+  //   console.log(espaco + asterisco);
   // }
 
-  for (let coluna = 1; saida.length < qt; coluna += 1) {
+  // for (let coluna = 1; asterisco.length < qt; coluna += 1) {
+  //   espaco = "";
+  //   asterisco = "";
+  //   for (let linha = 0; linha < (qt/2) - coluna; linha += 1) {
+  //     espaco += " ";
+  //   }
+  //   for (let restLinha = 0; restLinha < qt - (espaco.length * 2); restLinha += 1) {
+  //     asterisco += "*";
+  //   }
+  //   console.log(espaco + asterisco + espaco);
+  // }
+
+  for (let coluna = 1; asterisco.length < qt; coluna += 1) {
     espaco = "";
-    saida = "";
-    for (let linha = 0; linha < (qt/2) - coluna; linha += 1) {
+    asterisco = "";
+    for (let linha = 0; linha < qt / 2 - coluna; linha += 1) {
       espaco += " ";
     }
-    for (let restLinha = 0; restLinha < qt - (espaco.length * 2); restLinha += 1) {
-      saida += "*";
+    for (let restLinha = 0; restLinha < qt - espaco.length * 2; restLinha += 1) {
+      asterisco += "*";
+      if (asterisco.length < espaco.length * 2) {
+        for (let vazioCentro = espaco.length + asterisco.length; vazioCentro < qt - espaco.length - 1; vazioCentro += 1) {
+            asterisco += " ";
+            restLinha += 1;
+        }
+      }
     }
-    console.log(espaco + saida + espaco);
+    console.log(espaco + asterisco + espaco);
   }
 }
