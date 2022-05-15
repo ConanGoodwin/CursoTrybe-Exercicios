227ca085142ee31920157955fc28b73ffe14768c
Conan Goodwin <conan_goodwin@hotmail.com>
2022-05-15 18:13:17 -0300

    primeiros comandos em operacoes.js

---
+++ Md01_Fundamentos_Dev_web/Bloco04/operacoes.js
@@ -0,0 +1,49 @@
+let operador = "";
+let resultado = 0;
+let numero = 0;
+
+function operacao(numOuOp) {
+  console.log(numOuOp);
+
+  switch (numOuOp) {
+    case "z":
+      document.getElementById('operacaoAtual').innerText = "0";
+      break;
+    case "/":
+      operador = "/"
+      break;
+    case "*":
+      break;
+    case "-":
+      break;
+    case "+":
+      break;
+    case "=":
+      break;
+    case ",":
+      break;
+    case "1":
+      break;
+    case "2":
+      break;
+    case "3":
+      break;
+    case "4":
+      break;
+    case "5":
+      break;
+    case "6":
+      break;
+    case "7":
+      break;
+    case "8":
+      break;
+    case "9":
+      break;
+    case "0":
+  }
+}
+
+function calcula() {
+
+}
