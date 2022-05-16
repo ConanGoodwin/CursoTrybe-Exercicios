b4b207fb6a0656fc7c1dc0f16f047cc35e35719a
Conan Goodwin <conan_goodwin@hotmail.com>
2022-05-15 23:50:39 -0300

    codificado todas as operacoes em operacoes.js

--- Md01_Fundamentos_Dev_web/Bloco04/operacoes.js
+++ Md01_Fundamentos_Dev_web/Bloco04/operacoes.js
@@ -1,53 +1,30 @@
 let operador = "";
-let resultado = "";
-let numero = "";
 
 function operacao(numOuOp) {
   console.log(numOuOp);
   let textoAtual = document.getElementById("operacaoAtual").innerText;
 
   switch (numOuOp) {
+    case "=":
+      if (textoAtual != 0) {
+        document.getElementById("operacaoAnterior").innerText = textoAtual;
+        textoAtual = eval(textoAtual);
+        console.log(textoAtual);
+      }
+      break;
     case "z":
       textoAtual = "0";
       break;
-    case "/":
-      operador = "/";
-      textoAtual += operador;
-      break;
-    case "*":
-      operador = "*";
-      textoAtual += operador;
-      break;
-    case "-":
-      operador = "*";
-      textoAtual += operador;
-      break;
-    case "+":
-      operador = "+";
-      textoAtual += operador;
-      break;
-    case "=":
-      if (textoAtual === 0) {
-        textoAtual = numero;
-        operador = "";
-      } else {
-        textoAtual += operador + numero;
-        operador = "";
-        numero = ""
-      }
-      break;
-    case ",":
-      textoAtual += ".";
-      break;
     default:
       textoAtual += numOuOp;
+      if (textoAtual === "0/" || textoAtual === "0*" || textoAtual === "0-" || textoAtual === "0+") {
+        textoAtual = "0";
+      }
   }
 
   if (textoAtual[0] == "0" && textoAtual.length > 1) {
-    textoAtual = textoAtual.slice(1,textoAtual.length);
+    textoAtual = textoAtual.slice(1, textoAtual.length);
   }
 
-  document.getElementById("operacaoAtual").innerText = textoAtual;
+  document.getElementById("operacaoAtual").innerText = textoAtual;  
 }
-
-function calcula() {}
