1f613128597dfd83494fa18ffda85c7f81e6aede
Conan Goodwin <conan_goodwin@hotmail.com>
2022-05-17 00:45:47 -0300

    add asteriscoPiramede.js de acordo com o bonus 4

---
+++ Md01_Fundamentos_Dev_web/Bloco04/4.3/asteriscoPiramede.js
@@ -0,0 +1,21 @@
+let n = 6;
+let linha = "";
+let asterisco = 1;
+let emBranco = n / 2 - 1;
+
+for (let vertical = 0; vertical < n; vertical += 1) {
+  linha = "";
+  for (let caracter = 1; caracter <= n; caracter += 1) {
+    if (caracter > emBranco && caracter <= emBranco + asterisco) {
+      linha += "*";
+    } else {
+      linha += " ";
+    }
+  }
+  asterisco += 2;
+  emBranco -= 1;
+  console.log(linha);
+  if (emBranco < 0) {
+    break;
+  }
+}
