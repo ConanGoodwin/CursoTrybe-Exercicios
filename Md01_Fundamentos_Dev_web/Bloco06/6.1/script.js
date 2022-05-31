67567caff764c68a55ea72d8ac8e2f87796962c4
Conan Goodwin <conan_goodwin@hotmail.com>
2022-05-31 18:25:34 -0300

    conteudo parte IV requisito 01

---
+++ Md01_Fundamentos_Dev_web/Bloco06/6.1/script.js
@@ -0,0 +1,12 @@
+// SELETORES
+const INPUT_TEXT = document.querySelector("#input-text");
+const INPUT_CHECKBOX = document.querySelector("#input-checkbox");
+const HREF_LINK = document.querySelector("#href");
+
+function evitaPadrao(evento) {
+  const alvo = evento;
+
+  alvo.preventDefault();
+}
+
+HREF_LINK.addEventListener('click', evitaPadrao);
