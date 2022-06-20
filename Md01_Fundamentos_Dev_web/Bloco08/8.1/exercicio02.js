5205e892eec252d81a157dd1f00a55ee140e0ec6
Conan Goodwin <conan_goodwin@hotmail.com>
2022-06-20 00:51:11 -0300

    codado exercicios02.js

--- Md01_Fundamentos_Dev_web/Bloco08/8.1/exercicio02.js
+++ Md01_Fundamentos_Dev_web/Bloco08/8.1/exercicio02.js
@@ -1,4 +1,4 @@
 const sorteio = (aposta, funcao) =>
-  aposta == funcao() ? "Parabéns você ganhou!" : "Tente novamente.";
+  aposta === funcao() ? "Parabéns você ganhou!" : "Tente novamente.";
 
 console.log(sorteio(2, () => Math.floor(Math.random() * 5)));
