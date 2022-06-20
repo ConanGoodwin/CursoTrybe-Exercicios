5c88467a6b4d7864b4c1486c39f35c1966eec812
Conan Goodwin <conan_goodwin@hotmail.com>
2022-06-19 22:03:47 -0300

    corrigido loginValidation.test.js

--- Md01_Fundamentos_Dev_web/Bloco07/7.3/loginValidation.test.js
+++ Md01_Fundamentos_Dev_web/Bloco07/7.3/loginValidation.test.js
@@ -6,11 +6,9 @@ const {
 } = require("./loginValidation.js");
 
 describe("a função verifyCredentials()", () => {
-
   it("verifyCredentials() calls the correct function depending on the user and password input", () => {
-
     const user = {
-      userName: 'Bob',
+      userName: "Joana",
       password: 123456,
     };
 
@@ -20,13 +18,17 @@ describe("a função verifyCredentials()", () => {
       "Hello, Joana! Que bom ter você de volta"
     );
   });
+});
 
+describe("a função greetingMessage()", () => {
   it("greetingMessage() returns a message in the format: `Hello, ${user}! Que bom ter você de volta`", () => {
     expect(greetingMessage("Lucas")).toBe(
       "Hello, Lucas! Que bom ter você de volta"
     );
   });
+});
 
+describe("a função loginErrorMessage()", () => {
   it("loginErrorMessage() returns a message in the format: `Pessoa usuária '${user}' não encontrada, tente novamente!`", () => {
     expect(loginErrorMessage("Bob")).toBe(
       "Pessoa usuária 'Bob' não encontrada, tente novamente!"
