4f54dcf9e5a60af3461cfd3c747f23e20b5ee5d2
Conan Goodwin <conan_goodwin@hotmail.com>
2022-06-20 03:18:41 -0300

    codado megasena.js

--- Md01_Fundamentos_Dev_web/Bloco08/8.1/megasena.js
+++ Md01_Fundamentos_Dev_web/Bloco08/8.1/megasena.js
@@ -1,20 +1,22 @@
-const aposta = [2, 1, 3, 4, 5, 6].sort((a,b) => a - b);
+const aposta = [2, 1, 3, 4, 5, 6].sort((a, b) => a - b);
 const qtNumeros = aposta.length;
 const rangeMaximo = 7;
 
-const valida = (number1, number2) => number1.every((value, index) => value === number2[index]);
+const valida = (number1, number2) =>
+  number1.every((value, index) => value === number2[index]);
 
 const numAleatorio = () => Math.floor(Math.random() * rangeMaximo + 1);
 
 const sorteio = (apostado, funcao) => {
-
   const sorteia = (arra) => {
     if (arra.length < qtNumeros) {
       const geraNumero = (arrai, numero) => {
-        arrai.indexOf(numero) > -1 ? geraNumero(arrai, numAleatorio()) : arrai.push(numero);
+        arrai.indexOf(numero) > -1
+          ? geraNumero(arrai, numAleatorio())
+          : arrai.push(numero);
         return arrai;
-      }
-      arra = geraNumero(arra, numAleatorio()).sort((a,b) => a - b);
+      };
+      arra = geraNumero(arra, numAleatorio()).sort((a, b) => a - b);
       sorteia(arra);
     }
     return arra;
@@ -24,7 +26,9 @@ const sorteio = (apostado, funcao) => {
   console.log(`Numeros sorteados: ${sorte}`);
   console.log(`Numeros apostados: ${apostado}`);
 
-  return funcao(sorte, apostado) ? "acertou os numeros" : "Não acertou os numeros";
-}
+  return funcao(sorte, apostado)
+    ? "acertou os numeros"
+    : "Não acertou os numeros";
+};
 
-console.log(sorteio(aposta,valida));
+console.log(sorteio(aposta, valida));
