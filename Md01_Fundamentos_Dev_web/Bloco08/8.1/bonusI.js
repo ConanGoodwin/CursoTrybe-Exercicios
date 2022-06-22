07a9e55a3d4f1ea44d81e361b5137870268a71be
Conan Goodwin <conan_goodwin@hotmail.com>
2022-06-22 19:23:56 -0300

    codado bonusI.js requisito 03

--- Md01_Fundamentos_Dev_web/Bloco08/8.1/bonusI.js
+++ Md01_Fundamentos_Dev_web/Bloco08/8.1/bonusI.js
@@ -20,8 +20,37 @@ const dragon = {
 
 const battleMembers = { mage, warrior, dragon };
 
-const danoDragao = () => Math.floor(Math.random() * (dragon.strength - 15) + 16);
-const danoWarrior = () => Math.floor(Math.random() * (warrior.strength * warrior.weaponDmg - warrior.strength) + warrior.strength);
+const calculaValorAleatorio = (maximo, minimo) =>
+  Math.floor(Math.random() * maximo + minimo);
 
-console.log(danoDragao());
-console.log(danoWarrior());
+const calculaAtaque = (personagem) => {
+  let danoMaximo = 0;
+  let danoMinimo = 0;
+
+  switch (personagem) {
+    case "dragon":
+      danoMaximo = dragon.strength - 15;
+      danoMinimo = 16;
+      return calculaValorAleatorio(danoMaximo, danoMinimo);
+    case "warrior":
+      danoMaximo = warrior.strength * warrior.weaponDmg - warrior.strength;
+      danoMinimo = warrior.strength;
+      return calculaValorAleatorio(danoMaximo, danoMinimo);
+    case "mage":
+      if (mage.mana < 15) {
+        return "Não possui mana suficiente";
+      } else {
+        danoMaximo = mage.intelligence * 2 - mage.intelligence;
+        danoMinimo = mage.intelligence;
+        mage.mana = mage.mana - 15;
+        return {
+          dano: calculaValorAleatorio(danoMaximo, danoMinimo),
+          mana: mage.mana,
+        };
+      }
+  }
+};
+
+console.log(`Dano do Dragão: ${calculaAtaque("dragon")}`);
+console.log(`Dano do Guerreiro: ${calculaAtaque("warrior")}`);
+console.log(`Dano do Mago: ${mage.mana < 15 ? calculaAtaque("mage") : Object.entries(calculaAtaque("mage"))}`);
