84718113836eda597e7717423e470260b26bda11
Conan Goodwin <conan_goodwin@hotmail.com>
2022-06-26 18:45:48 -0300

    ok

--- Md01_Fundamentos_Dev_web/Bloco08/8.4/fatora.js
+++ Md01_Fundamentos_Dev_web/Bloco08/8.4/fatora.js
@@ -1,3 +1,3 @@
-const fatora = (numero, index) => (index > 1) ? fatora(numero * (index - 1), index - 1) : numero;
+const fatora = (numero, index = numero) => (index > 1) ? fatora(numero * (index - 1), index - 1) : numero;
 
-console.log(fatora(...[5,5]));
+console.log(fatora(4));
