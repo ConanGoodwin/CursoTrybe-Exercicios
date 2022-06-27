b59980a731b974803ede6705de14460edd104cf3
Conan Goodwin <conan_goodwin@hotmail.com>
2022-06-27 20:40:32 -0300

    alternativa exercicio01

--- Md01_Fundamentos_Dev_web/Bloco08/8.4/exercicio01.js.js
+++ Md01_Fundamentos_Dev_web/Bloco08/8.4/exercicio01.js.js
@@ -5,7 +5,12 @@ const arrays = [
 ];
 
 function flatten() {
-  return arrays.map((item) => item.reduce((acc, curr) => acc + curr));
+  // return arrays.map((item) => item.reduce((acc, curr) => acc + curr));
+  // return [...arrays[0], ...arrays[1], ...arrays[2]];
+  return arrays.reduce((acc, curr) => {
+    curr.forEach((item) => acc.push(item));
+    return acc;
+  }, []);
 }
 
 console.log(flatten());
