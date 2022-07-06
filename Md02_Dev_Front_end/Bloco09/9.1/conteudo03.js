7541ee78f8d3a4b998564d01147a6dcd09dd4636
Conan Goodwin <conan_goodwin@hotmail.com>
2022-07-06 15:30:11 -0300

    codado conteudos 1 2 e 3 .js

---
+++ Md02_Dev_Front_end/Bloco09/9.1/conteudo03.js
@@ -0,0 +1,24 @@
+// Definição da função userFullName
+const userFullName = ({ firstName, lastName }) => console.log(`Hello! My name is ${firstName} ${lastName}`);
+const userNationality = ({ firstName, nationality }) => console.log(`${firstName} is ${nationality}`);
+const delay = (maxMilliseconds = 5000) => Math.floor(Math.random() * maxMilliseconds);
+
+// Definição da função getUser
+const getUser = () => {
+  setTimeout(() => {
+    const user = {
+      firstName: "Ivan",
+      lastName: "Ivanovich",
+      nationality: "Russian",
+    };
+    // Retorne a `callback` passada como parâmetro na função `getUser`
+    // Dica: você pode manter o `console.log()`
+    // console.log(user);
+    return user;
+  }, delay());
+  // Retornamos nosso parâmetro, que será uma função (callback)
+};
+
+// Chamada/execução da função getUser, que vai receber como parâmetro nossa função userFullName.
+userFullName(getUser());
+userNationality(getUser());
