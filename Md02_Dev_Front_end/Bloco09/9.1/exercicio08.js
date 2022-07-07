80f5c540701b43054d888087ae7ba4e0f9617017
Conan Goodwin <conan_goodwin@hotmail.com>
2022-07-06 23:34:22 -0300

    codado exercicios de 8 a 9

---
+++ Md02_Dev_Front_end/Bloco09/9.1/exercicio08.js
@@ -0,0 +1,40 @@
+const pokemons = [
+  {
+    name: 'Bulbasaur',
+    type: 'Grass',
+    ability: 'Razor Leaf',
+  },
+  {
+    name: 'Charmander',
+    type: 'Fire',
+    ability: 'Ember',
+  },
+  {
+    name: 'Squirtle',
+    type: 'Water',
+    ability: 'Water Gun',
+  },
+];
+
+const showPokemon = (erro, message) => erro === null ? console.log(message) : console.log(erro);
+
+function getPokemonDetails(filter, callback) {
+  setTimeout(() => {
+    if (pokemons.find(filter) === undefined) {
+      return callback(new Error('Não temos esse pokémon para você :('), null);
+    }
+    const pokemon = pokemons.find(filter);
+
+    const { name, type, ability } = pokemon;
+
+    const messageFromProfOak = `Olá, seu pokémon é o ${name}, o tipo dele é ${type} e a habilidade principal dele é ${ability}`;
+
+    callback(null, messageFromProfOak);
+  }, 2000);
+}
+
+getPokemonDetails(({ name }) => name === 'Bulbasaur', showPokemon);
+
+module.exports = {
+  getPokemonDetails,
+};
