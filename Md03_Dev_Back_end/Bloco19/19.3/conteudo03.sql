647c4278b2a20bce0f158201ad32edc3298c5faa
ConanGoodwin <conan_goodwin@hotmail.com>
2022-10-17 18:28:10 -0300

    att conteudo03.sql 19.3

--- Md03_Dev_Back_end/Bloco19/19.3/conteudo03.sql
+++ Md03_Dev_Back_end/Bloco19/19.3/conteudo03.sql
@@ -17,5 +17,14 @@ SELECT email FROM customer WHERE address_id IN (172, 173, 174, 175, 176) ORDER B
 
 -- Descubra quantos pagamentos foram feitos entre 01/05/2005 e 01/08/2005. Lembre-se de que, no banco de dados,
 -- as datas estão armazenadas no formato ano/mês/dia, diferente do formato brasileiro, que é dia/mês/ano. 
+SELECT COUNT(*) FROM payment WHERE date(payment_date) BETWEEN '2005-05-01' AND '2005-08-01';
 
-SELECT COUNT(*) FROM payment WHERE payment_date BETWEEN '2005-05-01' AND '2005-08-01';
+-- Mostre o título, ano de lançamento e duração do empréstimo de todos os filmes com a duração de empréstimo de 3 a 6. 
+--Os resultados devem ser classificados em filmes com maior duração de empréstimo primeiro. 
+--Em caso de empate, ordene em ordem alfabética pelo título. 
+SELECT title, release_year, rental_duration FROM film WHERE rental_duration IN (3,4,5,6)
+ORDER BY rental_duration DESC, title;
+
+-- Monte um relatório que exiba o título e classificação dos 500 primeiros filmes
+-- direcionados para as classificações indicativas G, PG e PG-13. Os resultados devem estar ordenados por título. 
+SELECT title, rating FROM film WHERE rating IN ('G', 'PG', 'PG-13') ORDER BY title;
