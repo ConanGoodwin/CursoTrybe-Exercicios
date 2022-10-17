29dda7fac83687c2f9a7f11bab878a45ace8ff76
ConanGoodwin <conan_goodwin@hotmail.com>
2022-10-17 10:23:41 -0300

    revisando conteudo

--- Md03_Dev_Back_end/Bloco19/19.2/conteudo03.sql
+++ Md03_Dev_Back_end/Bloco19/19.2/conteudo03.sql
@@ -1,4 +1,11 @@
--- SELECT first_name AS 'fname' FROM sakila.actor 
--- WHERE (SELECT COUNT(*) FROM sakila.actor WHERE first_name='fname') > 1;
-USE sakila;
-SELECT first_name AS fname FROM (SELECT first_name AS fname2 FROM actor WHERE fname2=fname);
+-- Seleciona todas as pessoas que tem o mesmo nome, mesmo que tenham sobrenomes diferente
+SELECT first_name, last_name FROM sakila.actor a
+WHERE (SELECT COUNT(*) FROM sakila.actor b WHERE a.first_name=b.first_name) > 1 
+ORDER BY first_name;
+
+-- Seleciona todas as pessoas que tem o mesmo nome e mesmo sobrenome
+SELECT DISTINCT(first_name), last_name FROM sakila.actor a
+WHERE
+ (SELECT COUNT(*) FROM sakila.actor b
+ WHERE a.first_name=b.first_name
+ AND a.last_name=b.last_name) > 1;
