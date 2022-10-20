bff9af6b2410c38e6746be698b604118dc86ea33
ConanGoodwin <conan_goodwin@hotmail.com>
2022-10-20 20:30:15 -0300

    requisito 04 do exercicio extra dia 20.1

--- Md03_Dev_Back_end/Bloco20/exercicio_extra.sql
+++ Md03_Dev_Back_end/Bloco20/exercicio_extra.sql
@@ -8,10 +8,13 @@ SELECT * FROM film;
 SELECT MAX(length), MIN(length), AVG(length) FROM film WHERE release_year = 2006 GROUP BY rating;
 
 -- 3. Quantas pessoas usuárias temos por cidade (tabela address);
-SELECT COUNT(*), city_id FROM address GROUP BY city_id;
+SELECT COUNT(*), city FROM address a INNER JOIN city c ON a.city_id=c.city_id GROUP BY a.city_id;
 
 -- 4. Qual o valor total (amount) recebido dentro de cada mês (tabela payment)?
+SELECT * FROM payment;
+SELECT SUM(amount), month(payment_date) FROM payment GROUP BY month(payment_date);
 
 -- 5. Retorne a quantidade de itens alugados pelo preço (amount) que sejam menores ou iguais a 1.99 (tabela payment)
+
 
 -- 6. Qual valor arrecadado (soma) por valor de produto que tem mais saída por mês e ano? (tabela payment)
