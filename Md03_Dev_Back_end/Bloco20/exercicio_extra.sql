fc71b890a156f031103f6df7708a094a050d9c3a
ConanGoodwin <conan_goodwin@hotmail.com>
2022-10-21 03:39:13 -0300

    requisito 06 do exercicio extra dia 20.1 formatado

--- Md03_Dev_Back_end/Bloco20/exercicio_extra.sql
+++ Md03_Dev_Back_end/Bloco20/exercicio_extra.sql
@@ -27,8 +27,8 @@ SELECT max(x.qt) totQt, max(x.am) totRental, x.mm, x.yy FROM (
       INNER JOIN rental r ON p.rental_id=r.rental_id
       INNER JOIN inventory i ON r.inventory_id = i.inventory_id
       INNER JOIN film f ON i.film_id=f.film_id
-      GROUP BY i.film_id, month(p.payment_date), year(p.payment_date)
-      ORDER BY year(p.payment_date), month(p.payment_date), count(i.film_id) DESC
+    GROUP BY i.film_id, month(p.payment_date), year(p.payment_date)
+    ORDER BY year(p.payment_date), month(p.payment_date), count(i.film_id) DESC
 ) x 
 GROUP BY x.yy, x.mm, x.qt HAVING x.qt = (
   SELECT max(z.qt) FROM (
@@ -37,8 +37,8 @@ GROUP BY x.yy, x.mm, x.qt HAVING x.qt = (
       INNER JOIN rental r ON p.rental_id=r.rental_id
       INNER JOIN inventory i ON r.inventory_id = i.inventory_id
       INNER JOIN film f ON i.film_id=f.film_id
-      GROUP BY i.film_id, month(p.payment_date), year(p.payment_date)
-      ORDER BY year(p.payment_date), month(p.payment_date), count(i.film_id) DESC
+    GROUP BY i.film_id, month(p.payment_date), year(p.payment_date)
+    ORDER BY year(p.payment_date), month(p.payment_date), count(i.film_id) DESC
   ) z 
   WHERE z.mm = x.mm AND z.yy = x.yy
   GROUP BY z.yy, z.mm
