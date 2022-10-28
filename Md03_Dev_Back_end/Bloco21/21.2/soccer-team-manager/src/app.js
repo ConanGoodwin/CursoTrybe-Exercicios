ff6ec1bbdae2a4f08ac4434d970b5581ac466bde
ConanGoodwin <conan_goodwin@hotmail.com>
2022-10-28 17:16:02 -0300

    corrigido lint do conteudo do dia 21.2

--- Md03_Dev_Back_end/Bloco21/21.2/soccer-team-manager/src/app.js
+++ Md03_Dev_Back_end/Bloco21/21.2/soccer-team-manager/src/app.js
@@ -1,7 +1,7 @@
 // src/app.js
 const OK = 200;
-const INTERNAL_SERVER_ERROR = 500;
-const NOT_FOUND = 404;
+// const INTERNAL_SERVER_ERROR = 500;
+// const NOT_FOUND = 404;
 const express = require('express');
 
 const app = express();
@@ -38,60 +38,43 @@ app.get('/', (req, res) => res.status(OK).json({ message: 'Olá Mundo!' }));
 app.get('/teams', (req, res) => res.status(200).json({ teams }));
 app.get('/teams/:id', (req, res) => {
   const { id } = req.params;
-  const team = teams.find((team) => team.id === Number(id));
+  const teamEsp = teams.find((team) => team.id === Number(id));
 
-  res.status(200).json({ team });
+  res.status(200).json({ teamEsp });
 });
 
 app.post('/teams', (req, res) => {
-
   const newTeam = { ...req.body };
 
   teams.push(newTeam);
 
-
   res.status(201).json({ team: newTeam });
-
 });
 
 app.put('/teams/:id', (req, res) => {
-
   const { id } = req.params;
-
   const { name, initials } = req.body;
-
-
   const updateTeam = teams.find((team) => team.id === Number(id));
 
-
   if (!updateTeam) {
-
     res.status(404).json({ message: 'Team not found' });
-
   }
-
 
   updateTeam.name = name;
 
   updateTeam.initials = initials;
 
   res.status(200).json({ updateTeam });
-
 });
 
 app.delete('/teams/:id', (req, res) => {
-
   const { id } = req.params;
-
   const arrayPosition = teams.findIndex((team) => team.id === Number(id));
 
   teams.splice(arrayPosition, 1);
 
-
   res.status(200).end();
-
 });
-
 
 // GET / HTTP/1.1
 // Host: developer.mozilla.org
