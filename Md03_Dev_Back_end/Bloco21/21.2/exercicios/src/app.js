10c42dcb628e884e252fd01e803782cf133b3244
ConanGoodwin <conan_goodwin@hotmail.com>
2022-10-29 17:50:17 -0300

    add update

--- Md03_Dev_Back_end/Bloco21/21.2/exercicios/src/app.js
+++ Md03_Dev_Back_end/Bloco21/21.2/exercicios/src/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const { readFile, writeFile } = require('./utils/fsUtils');
 
 const app = express();
+app.use(express.json());
 
 async function main() {
   const readingMovies = await readFile('movies.json');
@@ -12,6 +13,16 @@ async function main() {
     const data = readingMovies.find(({ id }) => id === Number(idParams));
 
     return res.status(200).json(data);
+  });
+
+  app.get('/movies', (_req,res) => res.status(200).json(readingMovies));
+
+  app.post('/movies', (req,res) => {
+    const newMovie = { ...req.body };
+    // console.log(newMovie);
+
+    writeFile('movies.json', newMovie);
+    return res.status(200).end();
   });
 
   return readingMovies;
--- Md03_Dev_Back_end/Bloco21/21.2/exercicios/src/utils/fsUtils.js
+++ Md03_Dev_Back_end/Bloco21/21.2/exercicios/src/utils/fsUtils.js
@@ -16,14 +16,32 @@ const readFile = async (arqName) => {
 };
 
 const writeFile = async (arqName, objText) => {
-  try {
+  // try {
     const oldData = await readFile(arqName);
     // console.log(path.resolve(__dirname,`${FILE_PATH}/${arqName}`));
+    console.log(objText);
+    console.log(oldData.length);
+    console.log({ "id": oldData[oldData.length -1].id + 1, "movie": objText.movie, "price": objText.price});
 
-    fs.writeFile(path.resolve(__dirname,`${FILE_PATH}/${arqName}`), JSON.stringify([...oldData, objText]));  
-  } catch (error) {
-    confirm.error(error);
-  }
+    fs.writeFile(path.resolve(
+      __dirname,`${FILE_PATH}/${arqName}`),
+       JSON.stringify([...oldData, { "id": oldData[oldData.length -1].id + 1, "movie": objText.movie, "price": objText.price}]));  
+  // } catch (error) {
+  //   confirm.error(error);
+  // }
+}
+
+const updateFile = async (arqName, objText, id) => {
+  const oldData = await readFile(arqName);
+  const newData = oldData.reduce((acc,curr) => {
+    if (curr.id === id) {
+      return [...acc, objText];
+    } else {
+      return [...acc, curr];
+    }
+  })
+
+  fs.writeFile(path.resolve(__dirname,`${FILE_PATH}/${arqName}`), JSON.stringify(newData));
 }
 
 // const newText = {
