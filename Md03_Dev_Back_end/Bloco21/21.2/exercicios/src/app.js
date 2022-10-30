a8e1c13939bee35fe2a9c99984ac8bdf050e3eb5
ConanGoodwin <conan_goodwin@hotmail.com>
2022-10-30 00:50:44 -0300

    exrecicios bonus 21.2

--- Md03_Dev_Back_end/Bloco21/21.2/exercicios/src/app.js
+++ Md03_Dev_Back_end/Bloco21/21.2/exercicios/src/app.js
@@ -8,14 +8,21 @@ app.use(express.json());
 async function main() {
   const readingMovies = await readFile('movies.json');
 
+  app.get('/movies', (_req,res) => res.status(200).json(readingMovies));
+
+  app.get('/movies/search',(req,res) => {
+    const params = req.query;
+    const data = readingMovies.find(({movie}) => movie.toLowerCase().includes(params.q));
+
+    return res.status(200).json(data);
+  });
+
   app.get('/movies/:id',(req,res) => {
     const idParams = req.params.id;
     const data = readingMovies.find(({ id }) => id === Number(idParams));
 
     return res.status(200).json(data);
   });
-
-  app.get('/movies', (_req,res) => res.status(200).json(readingMovies));
 
   app.post('/movies', (req,res) => {
     const newMovie = { ...req.body };
