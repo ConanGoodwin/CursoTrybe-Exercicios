a7e77cc03416ec5ba43f5901d7089ced3f0aba0c
ConanGoodwin <conan_goodwin@hotmail.com>
2022-11-04 00:21:49 -0300

    config peopleDB.js e a rota post / para chamar o insert com o body da req

--- Md03_Dev_Back_end/Bloco21/21.5/trybecash-api/src/routes/peopleRoutes.js
+++ Md03_Dev_Back_end/Bloco21/21.5/trybecash-api/src/routes/peopleRoutes.js
@@ -1,11 +1,22 @@
 // src/routes/peopleRoutes.js
 const express = require('express');
+const { insert } = require('../database/peopleDB');
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   const person = req.body;
-  
+
+  try {
+    const [result] = await insert(person);
+
+    res.status(201).json({
+      message: `Pessoa cadastrada com sucesso com o id ${result.insertId}` });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Ocorreu um erro ao cadastrar uma pessoa' });
+  }
+
   res.status(201).json(person);
 });
 
