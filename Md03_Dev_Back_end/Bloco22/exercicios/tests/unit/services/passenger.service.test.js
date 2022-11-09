2cb0e2e03a3466a335c1ab20abad2f4d441fe3cb
ConanGoodwin <conan_goodwin@hotmail.com>
2022-11-09 16:43:34 -0300

    add testes passeger.service

---
+++ Md03_Dev_Back_end/Bloco22/exercicios/tests/unit/services/passenger.service.test.js
@@ -0,0 +1,25 @@
+// tests/unit/services/passenger.service.test.js
+const { describe } = require('chai');
+const sinon = require('sinon');
+
+const connection = require('../../../src/models/driver.model');
+
+describe('Verificando service pessoa passageira', function () {
+  describe('requisitando viagem', function () {
+    it('sem pontos de parada é realizada com sucesso', function () {
+      // arrange
+      // act
+      // assert
+    });
+    it('com pontos de parada é realizada com sucesso', async function () {
+      // arrange
+      // act
+      // assert
+    });
+    it('com mesmo local de origem e destino é rejeitada', async function () {
+      // arrange
+      // act
+      // assert
+    });
+  });
+});
