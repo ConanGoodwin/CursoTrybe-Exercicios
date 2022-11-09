f9d30f1666f9d13cda5be0ca98b2f0d8df7e3223
ConanGoodwin <conan_goodwin@hotmail.com>
2022-11-09 14:46:32 -0300

    pausa no exercicio 02

---
+++ Md03_Dev_Back_end/Bloco22/exercicios/tests/unit/models/car.model.test.js
@@ -0,0 +1,17 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+
+const connection = require('../../../src/models/connection');
+const { carModel } = require('../../../src/models');
+
+describe('Testes de unidade do model de carros', function () {
+  it('Insere um novo carro', async function () {
+    sinon.stub(connection, 'execute').resolves(1);
+
+    const result = await carModel.newCar();
+
+    expect(result).to.be.deep.equal(drivers); 
+  });
+
+  afterEach(sinon.restore);
+});
