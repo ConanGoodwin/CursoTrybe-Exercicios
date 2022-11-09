fa359bd485a829aee48561205176f8026e65b9c3
ConanGoodwin <conan_goodwin@hotmail.com>
2022-11-09 16:30:38 -0300

    add passager.service

---
+++ Md03_Dev_Back_end/Bloco22/exercicios/src/services/passenger.service.js
@@ -0,0 +1,31 @@
+const { travelModel, passagerModel, waypointModel } = require('../models');
+
+const doesPassengerExist = async (passengerId) => {
+  const passenger = await passagerModel.findById(passengerId);
+
+  if (passenger) return true;
+  return false;
+};
+
+const saveWaypoints = (waypoints, travelId) => {
+  if (waypoints && waypoints.length > 0) {
+    return waypoints.map(async (value) => await waypointModel.insert({ ...value, travelId }));
+  }
+  return [];
+};
+
+const requestTravel = async (passengerId, startingAddress, endingAddress, waypoints) => {
+  if (await doesPassengerExist(passengerId)) {
+    const travelId = await travelModel.insert({ passengerId, startingAddress, endingAddress });
+
+    await Promise.all(saveWaypoints(waypoints, travelId));
+    const travel = await travelModel.findById(travelId);
+
+    return travel;
+  }
+};
+
+
+module.exports = {
+  requestTravel,
+};
