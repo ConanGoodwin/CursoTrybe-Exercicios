998aef6132439151b74beea00be77e85eb12ffd1
ConanGoodwin <conan_goodwin@hotmail.com>
2022-11-09 02:45:05 -0300

    add index para a pasta models

---
+++ Md03_Dev_Back_end/Bloco22/exercicios/src/models/index.js
@@ -0,0 +1,5 @@
+const travelModel = require('./travel.model');
+
+module.exports = {
+  travelModel,
+}
--- Md03_Dev_Back_end/Bloco22/exercicios/tests/unit/models/travel.model.test.js
+++ Md03_Dev_Back_end/Bloco22/exercicios/tests/unit/models/travel.model.test.js
@@ -2,7 +2,7 @@ const { expect } = require('chai');
 const sinon = require('sinon');
 
 const connection = require('../../../src/models/connection');
-const travelModel = require('../../../src/models/travel.model');
+const { travelModel } = require('../../../src/models');
 
 const { travels, travelsFromDB } = require('./mocks/travel.model.mock');
 
