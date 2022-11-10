effbada79ff2266df15c3149bd6d21d4e41a5392
ConanGoodwin <conan_goodwin@hotmail.com>
2022-11-10 16:31:14 -0300

    refatorado para passar nos testes

--- Md03_Dev_Back_end/Bloco22/exercicios/src/controllers/passenger.controller.js
+++ Md03_Dev_Back_end/Bloco22/exercicios/src/controllers/passenger.controller.js
@@ -1,16 +1,18 @@
 // src/controllers/passenger.controller.js
-const { requestTravel } = require('../services/passenger.service');
+const passengerService = require('../services/passenger.service');
 const { mapError } = require('../utils/errorMap');
 
 const createTravel = async (req, res) => {
   const { passengerId } = req.params;
   const { startingAddress, endingAddress, waypoints } = req.body;
 
-  const { type, message } = await requestTravel(passengerId, startingAddress, endingAddress, waypoints);
+  const { type, message } = await passengerService.requestTravel(passengerId, startingAddress, endingAddress, waypoints);
 
   // Linha de código responsável por gerar uma resposta em caso de erro no
   // processamento do componente de software da camada `Service`
-  if (type) return res.status(mapError(type)).json(message);
+  if (type) {
+    return res.status(mapError(type)).json(message);
+  }
 
   res.status(201).json(message);
 };
