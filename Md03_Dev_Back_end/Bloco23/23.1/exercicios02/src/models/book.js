fa28a55b8dcdf1e934c7594218bc4c2ed5a0e7d2
ConanGoodwin <conan_goodwin@hotmail.com>
2022-11-26 20:39:18 -0300

    exercicio 02

--- Md03_Dev_Back_end/Bloco23/23.1/exercicios02/src/models/book.js
+++ Md03_Dev_Back_end/Bloco23/23.1/exercicios02/src/models/book.js
@@ -5,7 +5,9 @@ module.exports = (sequelize, DataTypes) => {
   const Book = define('Book', {
     title: DataTypes.STRING,
     author: DataTypes.STRING,
-    pageQuantity: DataTypes.INTEGER
+    pageQuantity: DataTypes.INTEGER,
+    createdAt: DataTypes.DATE,
+    updatedAt: DataTypes.DATE
   }, {
     sequelize,
     modelName: 'Book',
