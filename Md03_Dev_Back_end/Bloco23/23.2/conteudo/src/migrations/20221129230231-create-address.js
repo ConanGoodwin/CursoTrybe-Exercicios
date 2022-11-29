ea790da5b10aad78781064d5a86151f0a3dbd143
ConanGoodwin <conan_goodwin@hotmail.com>
2022-11-29 20:08:04 -0300

    conteudo migrate

---
+++ Md03_Dev_Back_end/Bloco23/23.2/conteudo/src/migrations/20221129230231-create-address.js
@@ -0,0 +1,40 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('addresses', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      city: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      street: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      number: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      employeeId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        field: 'employee_id',
+        references: {
+          model: 'employees',
+          key: 'id',
+        }
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('addresses');
+  }
+};
