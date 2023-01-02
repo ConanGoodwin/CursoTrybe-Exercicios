405164ff007d05dc26be7fb55d00a83e4bb62a7a
ConanGoodwin <conan_goodwin@hotmail.com>
2023-01-02 16:09:23 -0300

    exercicio 05

--- Md03_Dev_Back_end/Bloco25/25.1/my-ts-scripts/index.ts
+++ Md03_Dev_Back_end/Bloco25/25.1/my-ts-scripts/index.ts
@@ -1,7 +1,9 @@
 import * as Ex01 from './length';
 import * as Ex02 from './mass';
 import * as Ex03 from './capacity';
+import * as Ex04 from './area';
 
 console.log(`1 Kilometro em metros é: ${Ex01.convert(1,'km','m')}m`);
 console.log(`1 Quilograma em gramas é: ${Ex02.convert(1,'kg','g')}g`);
 console.log(`1 Quilolitro em litros é: ${Ex03.convert(1,'kl','l')}l`);
+console.log(`1 Quilômetro quadrado em metros quadrados é: ${Ex04.convert(1,'km²','m²')}m²`);
