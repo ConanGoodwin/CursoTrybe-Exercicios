86b9ee59a7bba26cd14b102b94dfc494645c0554
ConanGoodwin <conan_goodwin@hotmail.com>
2023-01-03 20:04:23 -0300

    finalizado conteudo na pratica

--- Md03_Dev_Back_end/Bloco25/25.2/conteudonaPratica/main.ts
+++ Md03_Dev_Back_end/Bloco25/25.2/conteudonaPratica/main.ts
@@ -1,9 +1,25 @@
-import connection from "./models/connection";
+// import connection from "./models/connection";
+
+// const main = async () => {
+//   const result = await connection.execute('SELECT * FROM books');
+//   const [rows] = result;
+//   console.log(rows);
+// }
+import readline from 'readline-sync'
+import BookModel, { Book } from "./models/Book";
 
 const main = async () => {
-  const result = await connection.execute('SELECT * FROM books');
-  const [rows] = result;
-  console.log(rows);
+  const bookModel = new BookModel();
+  const title = readline.question('Digite o título do livro: ');
+  const price = readline.questionFloat('Digite o preço do livro: ');
+  const author = readline.question('Digite o autor do livro: ');
+  const isbn = readline.question('Digite o isbn do livro: ');
+
+  const newBook: Book = { title, price, author, isbn };
+  const createNewBook = await bookModel.create(newBook);
+  const books = await bookModel.getAll();
+  console.log(createNewBook);
+  // console.log(books);
 }
 
 main();
