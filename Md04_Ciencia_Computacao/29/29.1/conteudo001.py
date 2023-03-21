9b8e66e8cb0aff91e5548ddcce013e0261c7ffa4
ConanGoodwin <conan_goodwin@hotmail.com>
2023-03-21 13:58:22 -0300

    feat: add exercicio 02, conteudo 001, dia 29.1

--- Md04_Ciencia_Computacao/29/29.1/conteudo001.py
+++ Md04_Ciencia_Computacao/29/29.1/conteudo001.py
@@ -1,3 +1,4 @@
+# exercicio 01
 a = 10
 b = 5
 print(a + b)
@@ -6,3 +7,11 @@ print(a * b)
 print(a / b)
 print(a // b)
 print(a % b)
+
+# exercicio 02
+hours = 6
+minutes = hours * 60
+seconds = minutes * 60
+print(minutes)
+print(seconds)
+
