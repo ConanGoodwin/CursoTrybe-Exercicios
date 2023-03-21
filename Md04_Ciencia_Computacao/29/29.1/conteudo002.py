ee9dc610df4e9de021afb35a43cc0f7017007d99
ConanGoodwin <conan_goodwin@hotmail.com>
2023-03-21 15:08:56 -0300

    feat: add conteudo 002, dia 29.1

---
+++ Md04_Ciencia_Computacao/29/29.1/conteudo002.py
@@ -0,0 +1,24 @@
+fruits = ["Laranja","maçã","uva","abacaxi"]
+fruits[0]
+# 'Laranja'
+fruits[-1]
+# 'abacaxi'
+fruits.append("banana")
+fruits.remove("abacaxi")
+fruits
+# ['Laranja', 'maçã', 'uva', 'banana']
+fruits.extend(["pera","melão","kiwi"])
+fruits
+# ['Laranja', 'maçã', 'uva', 'banana', 'pera', 'melão', 'kiwi']
+fruits.index("maçã")
+# 1
+fruits.index("teste")
+# Traceback (most recent call last):
+#  File "<stdin>", line 1, in <module>
+# ValueError: 'teste' is not in list
+fruits
+# ['Laranja', 'maçã', 'uva', 'banana', 'pera', 'melão', 'kiwi']
+fruits.sort()
+fruits
+# ['Laranja', 'banana', 'kiwi', 'maçã', 'melão', 'pera', 'uva']
+
