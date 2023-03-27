4c4278f0a061790a20907573a4632a45e93a0e66
ConanGoodwin <conan_goodwin@hotmail.com>
2023-03-27 01:15:41 -0300

    feat: add conteudo003.py dia 29.2

---
+++ Md04_Ciencia_Computacao/29/29.2/conteudo003.py
@@ -0,0 +1,34 @@
+#escrita
+with open("arquivo.txt",mode="w") as file:
+    file.write('Trybe S2')
+
+#leitura
+with open("arquivo.txt") as file:
+    content = file.read()
+    print(content)
+
+with open("arquivo.txt",mode="w") as file:
+    lines = ['Olá\n','mundo\n','belo\n','do\n','Pyhton\n']
+    file.writelines(lines)
+
+with open("arquivo.txt") as file:
+    for line in file:
+        print(line, end="")
+
+with open("arquivo.dat", mode="wb") as file:
+    file.write(b"C\xc3\xa1ssio 30")
+
+try:
+    with open("arquivos.dat",mode="rb") as file:
+        content = file.read()
+        print(content)
+except FileNotFoundError:
+    print("Não existe este arquivo")
+
+#Exercicio 03
+try:
+    with (open("alunos.txt",mode="r") as file_in, open("alunos-reprovados.txt",mode="w") as file_out):
+        reprovados = [ aluno.split(" ")[0] + "\n" for aluno in file_in if int((aluno.split(" ")[1]).replace("\n","")) < 6 ]
+        file_out.writelines(reprovados)
+except FileNotFoundError:
+    print("Não existe este arquivo")
