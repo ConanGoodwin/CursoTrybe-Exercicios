ab31830c852f9e29d252c8a75b3b69871eee923b
ConanGoodwin <conan_goodwin@hotmail.com>
2023-03-27 01:16:34 -0300

    feat: add conteudo003.py dia 29.2

--- Md04_Ciencia_Computacao/29/29.2/conteudo003.py
+++ Md04_Ciencia_Computacao/29/29.2/conteudo003.py
@@ -1,14 +1,14 @@
-#escrita
-with open("arquivo.txt",mode="w") as file:
-    file.write('Trybe S2')
+# escrita
+with open("arquivo.txt", mode="w") as file:
+    file.write("Trybe S2")
 
-#leitura
+# leitura
 with open("arquivo.txt") as file:
     content = file.read()
     print(content)
 
-with open("arquivo.txt",mode="w") as file:
-    lines = ['Olá\n','mundo\n','belo\n','do\n','Pyhton\n']
+with open("arquivo.txt", mode="w") as file:
+    lines = ["Olá\n", "mundo\n", "belo\n", "do\n", "Pyhton\n"]
     file.writelines(lines)
 
 with open("arquivo.txt") as file:
@@ -19,16 +19,23 @@ with open("arquivo.dat", mode="wb") as file:
     file.write(b"C\xc3\xa1ssio 30")
 
 try:
-    with open("arquivos.dat",mode="rb") as file:
+    with open("arquivos.dat", mode="rb") as file:
         content = file.read()
         print(content)
 except FileNotFoundError:
     print("Não existe este arquivo")
 
-#Exercicio 03
+# Exercicio 03
 try:
-    with (open("alunos.txt",mode="r") as file_in, open("alunos-reprovados.txt",mode="w") as file_out):
-        reprovados = [ aluno.split(" ")[0] + "\n" for aluno in file_in if int((aluno.split(" ")[1]).replace("\n","")) < 6 ]
+    with (
+        open("alunos.txt", mode="r") as file_in,
+        open("alunos-reprovados.txt", mode="w") as file_out,
+    ):
+        reprovados = [
+            aluno.split(" ")[0] + "\n"
+            for aluno in file_in
+            if int((aluno.split(" ")[1]).replace("\n", "")) < 6
+        ]
         file_out.writelines(reprovados)
 except FileNotFoundError:
     print("Não existe este arquivo")
