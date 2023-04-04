318fef38fa6a1ef7bcbce17761aa8661e14d0890
ConanGoodwin <conan_goodwin@hotmail.com>
2023-04-04 09:17:59 -0300

    feat: exercicio 02 a 04

--- Md04_Ciencia_Computacao/29/29.3/exrecicios.py
+++ Md04_Ciencia_Computacao/29/29.3/exrecicios.py
@@ -1,5 +1,8 @@
+import re
+
+
 # Exercicio 01
-def list_fizz_buzz(max):
+def list_fizz_buzz(max: int):
     result = []
     for num in range(1, max + 1):
         temp = ""
@@ -12,4 +15,45 @@ def list_fizz_buzz(max):
     return result
 
 
-print(list_fizz_buzz(15))
+# Exercicio 02
+def number_of_word(text):
+    result = ""
+    base_change = [
+        "ABC",
+        "DEF",
+        "GHI",
+        "JKL",
+        "MNO",
+        "PQRS",
+        "TUV",
+        "WXYZ",
+    ]
+    for letter in text:
+        for index, base in enumerate(base_change):
+            if letter.upper() in base:
+                result += str(index + 2)
+                break
+            elif index > 6:
+                result += letter
+    return result
+
+
+print(number_of_word("1-HOME-SWEET-HOME"))
+
+
+# Exercicio 03
+def validate_email(email):
+    regex = r"^[a-zA-Z]([\w\-\.]+)?\@[a-zA-Z]+\.[\w]{3}(\.[\w]{2})?$"
+    return bool(re.match(regex, email))
+
+
+# Exercicio 04
+def list_valid_email(list_email):
+    valid_email = []
+    for email in list_email:
+        if validate_email(email):
+            valid_email.append(email)
+        else:
+            print(email)
+
+    return valid_email
--- Md04_Ciencia_Computacao/29/29.3/test_exercicios.py
+++ Md04_Ciencia_Computacao/29/29.3/test_exercicios.py
@@ -1,4 +1,5 @@
-from exrecicios import list_fizz_buzz
+import pytest
+from exrecicios import list_fizz_buzz, list_valid_email, number_of_word, validate_email
 
 
 def test_list_fizz_buzz():
@@ -19,3 +20,50 @@ def test_list_fizz_buzz():
         14,
         "FizzBuzz",
     ]
+
+
+def test_list_fizz_buzz_not_number():
+    with pytest.raises(TypeError, match="can only concatenate str"):
+        list_fizz_buzz("a")
+
+
+def test_number_of_word():
+    assert number_of_word("1-HOME-SWEET-HOME") == "1-4663-79338-4663"
+
+
+def test_validate_email():
+    assert validate_email("con@test.com") is True
+
+
+def test_validate_email_not_with_dot_and_ext():
+    assert validate_email("con@test") is False
+
+
+def test_validate_email_not_with_ext():
+    assert validate_email("con@test.") is False
+
+
+def test_validate_email_not_with_arroba():
+    assert validate_email("contest.com") is False
+
+
+def test_validate_email_user_not_with_caracter():
+    assert validate_email("co&@test.com") is False
+
+
+def test_validate_email_user_not_with_first_caracter():
+    assert validate_email("1on@test.com") is False
+
+
+def test_validate_email_website_not_with_caracter():
+    assert validate_email("con@test1.com") is False
+
+
+def test_validate_email_ext_not_more_tree():
+    assert validate_email("con@test.coma") is False
+
+
+def test_list_valid_email():
+    assert list_valid_email(
+        ["nome@dominio.com", "errad#@dominio.com", "outro@dominio.com"]
+    ) == ["nome@dominio.com", "outro@dominio.com"]
